5b43ae5f99cba85452108d3653d1c05bcefacb0d
German Solano <kontrolg1239@gmail.com>
2020-08-18 07:41:35 -0400

    Remove while-loop in getGuessUntilFindSquareRoot

--- babylonian-method/answer.js
+++ babylonian-method/answer.js
@@ -1,29 +1,27 @@
+let tries;
+
 const squareRoot = radicand => {
-  if (radicand < 0) return NaN;
+  if (radicand < 0 || typeof radicand === "string") return NaN;
   const radicandHalf = radicand / 2;
   let guessedSquareRoot = getGuessBetween(0, radicandHalf);
-  let fallbackBreak = 0;
-  while (
-    fallbackBreak <= 100 &&
-    getNumberTimesTwo(guessedSquareRoot) !== radicand
-  ) {
-    guessedSquareRoot = getGuessUntilFindSquareRoot(
-      radicand,
-      guessedSquareRoot
-    );
-    fallbackBreak++;
-  }
-
-  return guessedSquareRoot;
+  tries = [];
+  return getGuessUntilFindSquareRoot(radicand, guessedSquareRoot);
 };
 
 const getGuessUntilFindSquareRoot = (radicand, previousGuess) => {
   const guessTimesTwo = getNumberTimesTwo(previousGuess);
-  if (guessTimesTwo !== radicand) {
-    const radicandDividedByGuess = radicand / previousGuess;
-    return (previousGuess + radicandDividedByGuess) / 2;
+  tries.push(previousGuess);
+  if (guessTimesTwo === radicand || tries.length > 50) {
+    return previousGuess;
+  } else {
+    return getAnotherGuess(radicand, previousGuess);
   }
-  return previousGuess;
+};
+
+const getAnotherGuess = (radicand, previousGuess) => {
+  const radicandDividedByGuess = radicand / previousGuess;
+  const newGuess = (previousGuess + radicandDividedByGuess) / 2;
+  return getGuessUntilFindSquareRoot(radicand, newGuess);
 };
 
 const getGuessBetween = (start, end) => {
@@ -32,6 +30,7 @@ const getGuessBetween = (start, end) => {
   return start + randomNumberBetweenDifference;
 };
 
-const getNumberTimesTwo = number => Math.pow(number, 2);
+const getNumberTimesTwo = number =>
+  Math.pow(number * 1000, 2) / Math.pow(1000, 2);
 
 module.exports = squareRoot;
